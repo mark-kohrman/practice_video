9343a1ad2fabcabd1fac06938f5029cb0bd9007c
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-04 19:09:08 -0600

    add are there duplicates solution

--- udemy.js
+++ udemy.js
@@ -1,33 +1,48 @@
-function sameFrequency(numberOne, numberTwo) {
-  let arrayOne = ("" + numberOne).split("");
-  let arrayTwo = ("" + numberTwo).split("");
-  let i = 0;
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  let numberFrequencies = {};
+// function sameFrequency(numberOne, numberTwo) {
+//   let arrayOne = ("" + numberOne).split("");
+//   let arrayTwo = ("" + numberTwo).split("");
+//   let i = 0;
+//   if (arrayOne.length !== arrayTwo.length) {
+//     return false;
+//   }
+//   let numberFrequencies = {};
 
-  while (i < arrayOne.length) {
-    let key1 = arrayOne[i]
-    if (numberFrequencies[key1] === undefined) {
-      numberFrequencies[key1] = 1;
-    } else {
-      numberFrequencies[key1] += 1;
+//   while (i < arrayOne.length) {
+//     let key1 = arrayOne[i]
+//     if (numberFrequencies[key1] === undefined) {
+//       numberFrequencies[key1] = 1;
+//     } else {
+//       numberFrequencies[key1] += 1;
+//     }
+//     i ++;
+//   }
+//   let j = 0
+//   while (j < arrayTwo.length) {
+//     let key2 = arrayTwo[j]
+//     if (numberFrequencies[key2] !== undefined && numberFrequencies[key2] > 0) {
+//       numberFrequencies[key2] -= 1;
+//     } else {
+//       return false;
+//     }
+//     j ++;
+//   }
+//   return true;
+// }
+// console.log(sameFrequency(1231, 3112))
+
+function areThereDuplicates() {
+  let frequencies = {};
+  let i;
+  
+  for (i = 0; i < arguments.length; i ++) {
+    let key = arguments[i];
+    if (frequencies[key] === undefined) {
+      frequencies[key] = 1;
+    } else  {
+      return true;
     }
-    i ++;
   }
-  let j = 0
-  while (j < arrayTwo.length) {
-    let key2 = arrayTwo[j]
-    if (numberFrequencies[key2] !== undefined && numberFrequencies[key2] > 0) {
-      numberFrequencies[key2] -= 1;
-      console.log(numberFrequencies)
-    } else {
-      return false;
-    }
-    j ++;
-  }
-  return true;
+  return false;
 }
-console.log(sameFrequency(1231, 3112))
+console.log(areThereDuplicates('a', 'b', 'c'))
 
